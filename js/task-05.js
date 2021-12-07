22298ae94e771a90052f14493a138635c327239c
Kindville <87278511+Kindville@users.noreply.github.com>
2021-12-07 14:15:25 +0200

    added changes to homework 6

--- js/task-05.js
+++ js/task-05.js
@@ -4,4 +4,7 @@ let outputNameEl = document.querySelector('#name-output');
 inputNameEl.addEventListener('input', (event) => {
   outputNameEl.textContent = event.currentTarget.value;
 
+  // inputNameEl === ' ' ? outputNameEl.textContent = 'Anonymous' : outputNameEl.textContent = event.currentTarget.value;
+    
+  
 });
